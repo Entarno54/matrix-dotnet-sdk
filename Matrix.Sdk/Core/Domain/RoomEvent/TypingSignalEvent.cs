9394757646582df27cd1755d934567bd0c4a0c93
Entar <entar57mailru@mail.ru>
2026-05-11 12:47:24 +0700

    Hai meowtrix bye matrix

--- Matrix.Sdk/Core/Domain/RoomEvent/TypingSignalEvent.cs
+++
@@ -1,3 +0,0 @@
-namespace Matrix.Sdk.Core.Domain.RoomEvent {
-    public record TypingSignalEvent(bool typing, uint timeout = 0);
-}
--- Matrix.Sdk/Core/Infrastructure/Dto/User/MatrixProfile.cs
+++
@@ -1,4 +0,0 @@
-namespace System.Runtime.CompilerServices.Dto.User
-{
-    public record MatrixProfile(string avatar_url, string displayname);
-}
